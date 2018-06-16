ce1f31f28cdbdd63e569c734539349a47fc4e8e8
zmajnex <zmajnex@yahoo.com>
2018-06-16 21:35:10 +0200

    Planer

--- inc/header.php
+++ inc/header.php
@@ -49,7 +49,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav mr-auto ">
             <li class="nav-item active">
-              <a class="nav-link text-info " href="index.html">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link text-info " href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link text-info " href="calendar.php">Calendar</a>
